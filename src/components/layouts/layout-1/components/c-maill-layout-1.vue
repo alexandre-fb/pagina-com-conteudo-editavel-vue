b92815b697b0e01ec52ffd899e21fbe34ebf09d7
Alexandre Bisognin <alexandrefb.86@gmail.com>
2022-10-18 15:47:31 -0300

    refactor

--- src/components/layouts/layout-1/components/c-maill-layout-1.vue
+++ src/components/layouts/layout-1/components/c-maill-layout-1.vue
@@ -266,8 +266,7 @@
                                         <tr style="font-size: 14px;">
                                             <td width="70%" style="padding: 20px 10px">
                                                 <div class="social-links" style="display: flex; gap: 10px; align-items: center;">
-                                                    <a href="https://pt-br.facebook.com/pinceisatlasoficial/" target="_blank">
-                                                        <i :style="{
+                                                    <a :style="{
                                                                 height: '20px', 
                                                                 width: '20px', 
                                                                 border: '1px solid #fff', 
@@ -275,42 +274,11 @@
                                                                 display: 'flex', 
                                                                 justifyContent: 'center', 
                                                                 aligItems: 'center',
-                                                                padding: '4px'
-                                                            }">
-                                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"/></svg>
-                                                        </i>
-                                                    </a>
-                                                    <a href="https://www.instagram.com/pinceisatlasoficial/" target="_blank">
-                                                        <i :style="{
-                                                                height: '20px', 
-                                                                width: '20px', 
-                                                                border: '1px solid #fff', 
-                                                                borderRadius: '100%', 
-                                                                display: 'flex', 
-                                                                justifyContent: 'center', 
-                                                                aligItems: 'center',
-                                                                padding: '4px'
-                                                            }">
-                                                            <svg class="instagramIcon" viewBox="0 0 448 512">
-                                                                <path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z" style="fill: #fff;"/>
-                                                            </svg>
-                                                        </i>
-                                                    </a>
-                                                    <a href="https://www.youtube.com/user/pinceisatlasoficial" target="_blank">
-                                                        <i :style="{
-                                                                height: '20px', 
-                                                                width: '20px', 
-                                                                border: '1px solid #fff', 
-                                                                borderRadius: '100%', 
-                                                                display: 'flex', 
-                                                                justifyContent: 'center', 
-                                                                aligItems: 'center',
-                                                                padding: '4px'
-                                                            }">
-                                                            <svg class="youtubeIcon" viewBox="0 0 576 512">
-                                                                <path d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z" style="fill: #fff;"/>
-                                                            </svg>
-                                                        </i>
+                                                                padding: socialLink.name === 'youtube' ? '5px' : '4px'
+                                                            }"  v-for="socialLink in socialLinks" :key="socialLink.index" href="https://pt-br.facebook.com/pinceisatlasoficial/" target="_blank">
+                                                        <img 
+                                                            :src="socialLink.imageUrl" 
+                                                            :alt="socialLink.alt">
                                                     </a>
                                                     <span>/picenisatlasoficial | <a href="https://www.pinceisatlas.com.br" target="_blank" style="color: #fff; text-decoration: none;">www.pinceisatlas.com.br</a></span>
                                                 </div> 
@@ -322,77 +290,6 @@
                                     </table>
                                 </td>
                             </tr>
-
-                            <!-- <tr class="footer-section" 
-                                :style="{
-                                    backgroundColor: data.primaryColor,
-                                    color: '#fff',
-                                }">
-                                <td style="
-                                        display: flex; 
-                                        justify-content: space-between; 
-                                        align-items: center; 
-                                        padding: 15px 10px;
-                                        max-width: 600px;
-                                        margin: 0 auto;
-                                    "
-                                >
-                                    <tr>
-                                        <td>links</td>
-                                        <td>logo</td>
-                                    </tr>
-                                    <div class="social-links" style="display: flex; gap: 10px; align-items: center;">
-                                        <a href="https://pt-br.facebook.com/pinceisatlasoficial/" target="_blank">
-                                            <i :style="{
-                                                    height: '20px', 
-                                                    width: '20px', 
-                                                    border: '1px solid #fff', 
-                                                    borderRadius: '100%', 
-                                                    display: 'flex', 
-                                                    justifyContent: 'center', 
-                                                    aligItems: 'center',
-                                                    padding: '4px'
-                                                }">
-                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"/></svg>
-                                            </i>
-                                        </a>
-                                        <a href="https://www.instagram.com/pinceisatlasoficial/" target="_blank">
-                                            <i :style="{
-                                                    height: '20px', 
-                                                    width: '20px', 
-                                                    border: '1px solid #fff', 
-                                                    borderRadius: '100%', 
-                                                    display: 'flex', 
-                                                    justifyContent: 'center', 
-                                                    aligItems: 'center',
-                                                    padding: '4px'
-                                                }">
-                                                <svg class="instagramIcon" viewBox="0 0 448 512">
-                                                    <path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z" style="fill: #fff;"/>
-                                                </svg>
-                                            </i>
-                                        </a>
-                                        <a href="https://www.youtube.com/user/pinceisatlasoficial" target="_blank">
-                                            <i :style="{
-                                                    height: '20px', 
-                                                    width: '20px', 
-                                                    border: '1px solid #fff', 
-                                                    borderRadius: '100%', 
-                                                    display: 'flex', 
-                                                    justifyContent: 'center', 
-                                                    aligItems: 'center',
-                                                    padding: '4px'
-                                                }">
-                                                <svg class="youtubeIcon" viewBox="0 0 576 512">
-                                                    <path d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z" style="fill: #fff;"/>
-                                                </svg>
-                                            </i>
-                                        </a>
-                                        <span>/picenisatlasoficial | <a href="https://www.pinceisatlas.com.br" target="_blank" style="color: #fff; text-decoration: none;">www.pinceisatlas.com.br</a></span>
-                                    </div>  -->
-                                    <!-- <img :src="data.logoImageUrl" alt="Logo Atlas" style="object-fit: contain; width: 130px;"> -->
-                                <!-- </td>
-                            </tr> -->
                         </table>
                     </td>
                 </tr>
@@ -419,6 +316,24 @@ export default {
             showEditBannerSectionButton: false,
             showEditDescriptionSectionButton: false,
             showEditProductsListSectionButton: false,
+            socialLinks: [
+                {
+                    imageUrl: 'https://github.com/alexandre-fb/pagina-com-conteudo-editavel-vue/blob/main/src/assets/icons/facebook-icon.png?raw=true',
+                    alt:'Ícone do Facebook',
+                    link: 'https://pt-br.facebook.com/pinceisatlasoficial/'
+                },
+                {
+                    imageUrl: 'https://github.com/alexandre-fb/pagina-com-conteudo-editavel-vue/blob/main/src/assets/icons/instagram-icon.png?raw=true',
+                    alt:'Ícone do Instagram',
+                    link: 'https://pt-br.facebook.com/pinceisatlasoficial/'
+                },
+                {
+                    imageUrl: 'https://github.com/alexandre-fb/pagina-com-conteudo-editavel-vue/blob/main/src/assets/icons/youtube-icon.png?raw=true',
+                    alt:'Ícone do Youtube',
+                    link: 'https://pt-br.facebook.com/pinceisatlasoficial/',
+                    name: 'youtube'
+                },
+            ]
         }
     },
     watch: {
